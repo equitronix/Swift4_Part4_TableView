5630697b579a30d924e6e7e4ab8bcd2d560bdee8
Arun Saxena <admin@admins-macbook-pro.local>
2018-11-17 15:58:19 +0530

    Saving images as jpeg to disk.

--- Swift4_Part4_TableView/ImageStore.swift
+++ Swift4_Part4_TableView/ImageStore.swift
@@ -14,11 +14,37 @@ class ImageStore{
     
     func setImage(forImage image: UIImage, forKey key: String){
         cache.setObject(image, forKey: key as NSString)
+        
+        let url = imageURL(forKey: key);
+        if let data = image.jpegData(compressionQuality: 0.5) {
+            try? data.write(to: url, options: .atomic);
+        }
     }
     func getImage(forKey key: String) -> UIImage?{
-        return cache.object(forKey: key as NSString)
+        if let existingImage = cache.object(forKey: key as NSString) {
+            return existingImage;
+        }
+        let url = imageURL(forKey: key);
+        guard let imageFromDisk = UIImage(contentsOfFile: url.path) else { return nil;}
+        
+        cache.setObject(imageFromDisk, forKey: key as NSString);
+        return imageFromDisk;
+        
+        
     }
     func deleteImage(forKey key: String){
         cache.removeObject(forKey: key as NSString)
+        let url = imageURL(forKey: key);
+        do {
+            try FileManager.default.removeItem(at: url);
+        }catch {
+            print("could not save image to disk \(error)");
+        }
+    }
+    
+    func imageURL(forKey key: String) ->URL{
+        let documentDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentDirectories.first!
+        return documentDirectory.appendingPathComponent(key);
     }
 }
